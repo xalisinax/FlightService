fc1bba13e4f4ade7fe16e981485f45c0db0b3a04
xalisinax <cjalisina@gmail.com>
2025-02-17 04:53:28 +0330

    added providers project

---
+++ FlightService.Infrastructure.Provision/Emails/FakeEmailProvider.cs
@@ -0,0 +1,17 @@
+﻿using FlightService.Core.Domain.Common.DI;
+using FlightService.Core.Domain.Common.Providers.EMail;
+using Microsoft.Extensions.Logging;
+
+namespace FlightService.Infrastructure.Provision.Emails;
+
+internal class FakeEmailProvider(ILogger<FakeEmailProvider> logger) : IEmailProvider, ISingleton
+{
+    private readonly ILogger<FakeEmailProvider> _logger = logger;
+
+    public Task Send(string to, string subject, string body)
+    {
+        _logger.LogCritical("Email sent to : {to} with subject : {subject} and body of {body} \r", to, subject, body);
+
+        return Task.CompletedTask;
+    }
+}
