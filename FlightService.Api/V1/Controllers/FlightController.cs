a480dc8813d0d89311949375b2506fd76a081a0e
xalisinax <cjalisina@gmail.com>
2025-02-17 04:53:07 +0330

    folder moved

--- FlightService.Api/V1/Controllers/FlightController.cs
+++
@@ -1,9 +0,0 @@
-﻿using FlighService.Core.Domain.Common.MVC;
-using Microsoft.AspNetCore.Mvc;
-
-namespace FlightService.Api.V1.Controllers;
-
-[Route("v1/[controller]")]
-public class FlightController : ParcellController
-{
-}
